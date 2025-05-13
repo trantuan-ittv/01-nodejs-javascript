2d99c5ad0e87f391e4e9d925f48714283aef7435
Tuan Tran <trantuan.ittv@gmail.com>
2025-05-13 23:50:38 +0700

    step 11

---
+++ src/middleware/auth.js
@@ -0,0 +1,32 @@
+require("dotenv").config();   // goi de dung phia duoi process.env.JWT_SECRET
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+        const white_lists = ["/", "/register", "/login"]    // danh sach ko check auth cho di tiep
+        if(white_lists.find(item => '/v1/api' + item === req.originalUrl)){
+            next();
+        }else {
+            if(req?.headers?.authorization?.split(' ')?.[1]){                   // if(req.headers && req.headers.authorization)  // viet kieu tuong minh hoac tat nhu kia
+                const token = req.headers.authorization.split(' ')[1];
+                //verify
+                try {
+                    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+                    console.log(">>> check token", decoded);
+                    next();
+                } catch (error) {
+                    return res.status(401).json({
+                        message: "Token bị hết hạn/Hoặc không hợp lệ"
+                    })
+                }
+                
+            }else {
+                // return exception
+                return res.status(401).json({
+                    message: "Bạn chưa truyền Access Token ở header/Hoặc token bị hết hạn"
+                })
+            }
+        }
+
+}
+
+module.exports = auth;
---
+++ src/routes/api.js
@@ -0,0 +1,18 @@
+// api nay theo tuna tuong duong route web
+const express = require('express');
+const { createUser, handleLogin, getUser } = require('../controllers/userController');
+const auth = require('../middleware/auth');
+
+const routerAPI = express.Router();
+routerAPI.all("*", auth); // tat ca cac route xe chay qua auth
+
+routerAPI.get("/", (req, res) => {
+    return res.status(200).json("Hello world api tuna call form backend")
+})
+
+routerAPI.post("/register", createUser)
+routerAPI.post("/login", handleLogin)
+routerAPI.get("/user", getUser)
+
+
+module.exports = routerAPI; //export default
